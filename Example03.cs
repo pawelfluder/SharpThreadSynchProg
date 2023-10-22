d2e498981efcd2e7f5d389fd93efb5b5024df9e0
Build Service <buildservice@mvpprojects.com>
2023-10-22 14:35:02 +0200

    update

---
+++ Example03.cs
@@ -0,0 +1,34 @@
+﻿namespace SharpThreadSynchProg
+{
+    internal class Example03
+    {
+        private AutoResetEvent _are = new AutoResetEvent(true);
+
+        public void Main()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                new Thread(WriteTask).Start();
+            }
+        }
+
+        public void WriteTask()
+        {
+            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} Write task wating..");
+            _are.WaitOne();
+            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} Write task doing..");
+            WriteJob();
+            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} Write task completed..");
+            _are.Set();
+        }
+
+        private void WriteJob()
+        {
+            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} Write job started..");
+            Thread.Sleep(5 * 1000);
+            //throw new Exception();
+            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} Write job completed..");
+        }
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -2,18 +2,20 @@
 {
     public static class Program
     {
-
         public static void Main(string[] args)
         {
-            var example01 = new Example01();
+            //var example01 = new Example01();
             //example01.Main(example01.Case01);
             //example01.Main(example01.Case02);
             //example01.Main(example01.Case03);
             //example01.Main(example01.Case04);
-            
-            var example02 = new Example02();
-            example02.Main();
-            
+
+            //var example02 = new Example02();
+            //example02.Main();
+
+            var example03 = new Example03();
+            example03.Main();
+
             Console.ReadLine();
         }
     }
